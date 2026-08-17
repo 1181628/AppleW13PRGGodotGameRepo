66e00c0298253e69106faf118f5dc3fc450e31e0
1181628 <AppleWang1181628@gmail.com>
2026-08-17 15:01:33 +1200

    v1.5: New Animations
    Throughout the weekend, I prepared more animations for the player and imported them into Godot. However, I encountered a bug because my original test animation was named “trial,” while the completed animation had a different name. Some parts of the project were still referring to the old name, which caused the game to crash. I fixed the naming problem and then began programming the player’s attack system, but I did not have enough time to complete it.

--- GodotExtension/src/player.h
+++ GodotExtension/src/player.h
@@ -3,17 +3,39 @@
 
 #include <godot_cpp/classes/character_body2d.hpp>
 #include <godot_cpp/variant/vector2.hpp>
+#include <godot_cpp/classes/animation_player.hpp>
 
 namespace godot {
 
 class Player : public CharacterBody2D {
-	GDCLASS(Player, CharacterBody2D)
+	GDCLASS(Player, CharacterBody2D);
 
 private:
 	double gravity = 580.0;
     double horizontal_acceleration = 2000.0;
 	double maxHorizontalSpeed = 120.0;
 	double jumpSpeed = 210.0;
+
+	// all possible player states
+	enum class State {
+        NORMAL,
+        DASH,
+        ATTACK,
+        ATTACK_UP,
+        ATTACK_DOWN
+    };
+	// record the player's initial state
+	State current_state = State::NORMAL;
+
+	AnimationPlayer * animation_player = nullptr;
+
+	void process_normal(double delta);
+    void process_dash(double delta);
+    void process_attack(double delta);
+    void process_attack_up(double delta);
+    void process_attack_down(double delta);
+
+    void _update_animation();
 
 protected:
 	static void _bind_methods();
@@ -24,7 +46,6 @@ public:
 
 	void _ready() override;
 	void _physics_process(double delta) override;
-	void _update_animation();
 };
 
 }
