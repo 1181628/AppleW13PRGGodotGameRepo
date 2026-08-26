dc0dc4ccb6425ebf192e4c43954d15212894e982
Apple Wang <AppleWang1181628@gmail.com>
2026-08-26 15:19:32 +1200

    v1.9: Hit Detection
    Fixed a small problem that prevents the idle animation from not working. Although it was a minor error, I had encountered the same problem twice and it took me quite a time. I then began creating a combat detection system using the player’s hitbox and the enemy’s hurtbox. The goal is for the enemy&player to detect the player&enemy's attack and take damage when the two areas overlap. However, the collision detection is not working correctly yet, so I still need to investigate and fix it.

--- GodotExtension/src/enemy1.h
+++ GodotExtension/src/enemy1.h
@@ -1,11 +1,10 @@
 #ifndef ENEMY1_H
 #define ENEMY1_H
 
-#include <godot_cpp/classes/node.hpp>
-
-// Add other Godot classes or data types when needed.
-// #include <godot_cpp/variant/vector2.hpp>
-// #include <godot_cpp/variant/string.hpp>
+#include <godot_cpp/classes/character_body2d.hpp>
+#include <godot_cpp/classes/area2d.hpp>
+#include <godot_cpp/classes/sprite2d.hpp>
+#include <godot_cpp/classes/timer.hpp>
 
 namespace godot {
 
@@ -23,13 +22,14 @@ protected:
     static void _bind_methods();
 
 public:
-    NewClass();
-    ~NewClass();
+    Enemy1();
+    ~Enemy1();
 
     // Keep only the Godot callbacks that the class needs.
     void _ready() override;
     void _process(double delta) override;
-    // void _physics_process(double delta) override;
+    void _on_hurtbox_area_entered(godot::Area2D *area);
+    void _on_material_timer_timeout();
 
     // Declare custom functions here.
     // void take_damage(int damage);
